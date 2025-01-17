2e41b3835ec8e6da8069103b37b831f70c833862
Pratik Kumbhar <142420903+Pratik5357@users.noreply.github.com>
2025-01-17 18:37:33 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -24,7 +24,7 @@ export default function Signup() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:3000/api/users/signup', data);
+            const res = await axios.post('https://trade-vault-nextjs.vercel.app/api/users/signup', data);
             console.log(res.data);
             setError('');
             router.push('/sign-in');
